b5ba95f12bc699610e39c22daf0eb8695e1ea284
Malinov Alexey <malinov_av@mail.ru>
2022-11-01 02:41:54 +0300

    My First Commit

---
+++ first_program.cpp
@@ -0,0 +1,9 @@
+// This file contains the 'main' function. Program execution begins and ends there.
+// Public project
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello, Git!!!";
+    return 0;
+}
